fc986948e0bf21a806d1866d27d38bfc8ee02ee6
Uddeshya Shukla <uddeshyashukla88@gmail.com>
2025-03-29 18:40:19 +0530

    Completed First Phase

---
+++ src/main/java/forms/ContactSearchForm.java
@@ -0,0 +1,13 @@
+package forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContactSearchForm {
+
+    private String field;
+    private String value;
+
+}
---
+++ src/main/java/helpers/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package helpers;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
+}
